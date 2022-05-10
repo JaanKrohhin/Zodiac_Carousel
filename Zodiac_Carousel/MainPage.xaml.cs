c6e518a38088fe0e908c9bdb7f765ccad07ff8a3
opilane <opilane@HP-CZC6297BB7>
2022-05-10 12:20:42 +0300

    Add project files.

---
+++ Zodiac_Carousel/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Zodiac_Carousel
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            var r = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label{ Text = "Red", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)), HorizontalOptions = LayoutOptions.Center},
+                        new BoxView{ Color = Color.Red, WidthRequest = 200, HeightRequest = 200, HorizontalOptions = LayoutOptions.Center }
+                    }
+                }
+            };
+            var g = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label{ Text = "Green", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)), HorizontalOptions = LayoutOptions.Center},
+                        new BoxView{ Color = Color.Green, WidthRequest = 200, HeightRequest = 200, HorizontalOptions = LayoutOptions.Center }
+                    }
+                }
+            };
+            var b = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label{ Text = "Blue", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)), HorizontalOptions = LayoutOptions.Center},
+                        new BoxView{ Color = Color.Blue, WidthRequest = 200, HeightRequest = 200, HorizontalOptions = LayoutOptions.Center }
+                    }
+                }
+            };
+            for (int i = 0; i < 12; i++)
+            {
+                Children.Add(new Zo(i));
+            }
+        }
+    }
+}
---
+++ Zodiac_Carousel/Zo.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Zodiac_Carousel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Zo : ContentPage
+    {
+        public Zo(int _index)
+        {
+            InitializeComponent();
+            BindingContext = this;
+            en.Text = Properties.Resources.ResourceManager.GetString("_" + _index.ToString());
+            img.Source = ImageSource.FromResource("Zodiac_Carousel.Resources._" + _index.ToString() + ".jpg");
+        }
+    }
+}
